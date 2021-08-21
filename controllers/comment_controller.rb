b3c685a5e2c1db36dba924a94632723b16c4dfa8
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-08-21 21:55:08 +0700

    feat(attachment): allow users to attach files when creating comment

--- controllers/comment_controller.rb
+++ controllers/comment_controller.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'securerandom'
 
 require './models/comment.rb'
 
@@ -6,6 +7,17 @@ require './models/comment.rb'
 class CommentController
   def self.create(params)
     hashtags = extract_hashtags(params['content'])
+
+    is_with_file = !params['attachment'].nil?
+    file_path = nil
+    if is_with_file
+      file_info = params['attachment']
+      filename = file_info['filename']
+      file_ext = File.extname(filename)
+      generated_random_filename = SecureRandom.urlsafe_base64() + file_ext
+      file_content = file_info['tempfile']
+      file_path = "public/#{generated_random_filename}"
+    end
 
     comment = Comment.new({
       user_id: params['user_id'],
@@ -16,8 +28,16 @@ class CommentController
     })
 
     begin
+      if is_with_file
+        File.open(file_path, 'wb') do |file|
+          file.write(file_content.read())
+        end
+      end
       comment.save()
     rescue Exception => e
+      if is_with_file
+        File.delete(file_path)
+      end
       return [400, {error: e.to_s}.to_json()]
     end
 
